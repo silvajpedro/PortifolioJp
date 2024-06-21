f5556573731fe1daed80e2f89ce86f6f6369ebc5
silvajpedro <belojpedro.31@gmail.comcear>
2024-06-21 17:35:56 -0300

    :sparkles: feat: Adicionado React-responsive-carousel

--- pages/Projects/Carousel.js
+++ pages/Projects/Carousel.js
@@ -13,7 +13,6 @@ export default function Carrosel({ children }) {
             infiniteLoop={true}
             autoPlay={true}
             className={styles.Carrosel}
-
         >
 
             {children}
